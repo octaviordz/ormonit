231c7c7af672859c9a47cb91470dd1c13371c913
Octavio Rodríguez <octavio.rdz@outlook.com>
2017-02-26 21:56:26 -0700

    Add logicId configuration argument. Update command processing, now known as note processing.  Change ipc language from "signal" to "note"

--- src/Hamster.DbMonitor/Control.fs
+++ src/Hamster.DbMonitor/Control.fs
@@ -39,38 +39,48 @@ let on_hamster_db_change (e:FileSystemEventArgs) : unit =
     sprintf "Source \"%s\" copied to destination \"%s\"." e.FullPath destinationDbPath |> log.Trace
     ()
 
-let ctrlloop (config : IDictionary<string, string>) =
-    let ca = config.["controlAddress"]
-    let da = config.["dataAddress"]
-    sprintf "In control loop with: controlAddress \"%s\", dataAddress \"%s\"." ca da |> log.Trace
+let ctrlloop (lid:int) (ca:string) =
+    sprintf "In control loop with: controlAddress \"%s\", logicId \"%d\"." ca lid |> log.Trace
     let s = NN.Socket(Domain.SP, Protocol.RESPONDENT)
     //TODO:error handling for socket and connect
     assert (s >= 0)
     assert (NN.Connect(s, ca) >= 0)
-    let rec recvloop config =
+    let rec recvloop () =
         //let mutable buff : byte[] = null
         //buff is initialized with '\000'
         let buff : byte array = Array.zeroCreate 256
-        log.Info("Waiting for command.")
+        log.Info("Waiting for note.")
         let rc = NN.Recv(s, buff, SendRecvFlags.NONE)
         //TODO:Error handling NN.Errno ()
         assert (rc >= 0)
         let cmd = Encoding.UTF8.GetString(buff.[..rc - 1])
-        sprintf "\"%s\" command received." cmd |> log.Trace
+        sprintf "\"%s\" note received." cmd |> log.Trace
         match cmd with
-        | "stop" ->
-            let nbytes = NN.Send(s, [|0uy|], SendRecvFlags.NONE)
-            assert(nbytes = 1)
+        | "close" ->
+            let bytes = Array.zeroCreate 8
+            Buffer.BlockCopy(BitConverter.GetBytes(lid), 0, bytes, 0, 4)
+            Buffer.BlockCopy(BitConverter.GetBytes(0), 0, bytes, 4, 4)
+            log.Trace("""[{0}] Sending "close" aknowledgement.""", lid)
+            let sr = NN.Send(s, bytes, SendRecvFlags.NONE)
+            assert (sr > 0)
             ()
         | "report-status" ->
-            let sr = NN.Send(s, [|1uy|], SendRecvFlags.NONE)
+            let bytes = Array.zeroCreate 8
+            Buffer.BlockCopy(BitConverter.GetBytes(lid), 0, bytes, 0, 4)
+            Buffer.BlockCopy(BitConverter.GetBytes(0), 0, bytes, 4, 4)
+            log.Trace("""[{0}] Sending "report-status" aknowledgement.""", lid)
+            let sr = NN.Send(s, bytes, SendRecvFlags.NONE)
             //TODO:Error handling NN.Errno ()
-            assert (sr >= 0)
-            recvloop config
-        | _ -> recvloop config
-    recvloop config
+            assert (sr > 0)
+            recvloop ()
+        | _ -> recvloop ()
+    recvloop ()
 
-let Start (config : IDictionary<string, string>) =
+let Start (config:IDictionary<string, string>) =
+    let ca = config.["controlAddress"]
+    let p, lid = Int32.TryParse config.["logicId"]
+    if not p then
+        raise (System.ArgumentException("logicId"))
     //sprintf "Start with configuration %A" config |> log.Trace
     //%APPDATA%\Roaming\hamster-applet
     let app_data = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
@@ -89,7 +99,7 @@ let Start (config : IDictionary<string, string>) =
     //log.Trace("Begin watching.")
     watcher.EnableRaisingEvents <- true
     //log.Info("Enter control loop.")
-    ctrlloop config
+    ctrlloop lid ca
     log.Info("Exit control loop.")
     watcher.Dispose()
 
