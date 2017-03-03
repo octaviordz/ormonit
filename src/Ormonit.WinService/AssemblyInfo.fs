52543cde6f521c036ff21cae42cd9199edc45b4b
Octavio Rodríguez <octavio.rdz@outlook.com>
2017-03-02 21:45:43 -0700

    Add Windows Service host project.

--- src/Ormonit.WinService/AssemblyInfo.fs
+++ src/Ormonit.WinService/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace Ormonit.Service.AssemblyInfo
+﻿namespace Ormonit.WinService.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("Ormonit.Service")>]
+[<assembly: AssemblyTitle("Ormonit.WinService")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Ormonit.Service")>]
+[<assembly: AssemblyProduct("Ormonit.WinService")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("2d70564c-1857-4135-a01f-c2a758f7669b")>]
+[<assembly: Guid("0bde82e5-6930-4d50-b076-d6d4618a2260")>]
 
 // Version information for an assembly consists of the following four values:
 // 
---
+++ src/Ormonit.WinService/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+open System.IO
+open System.Diagnostics
+open Topshelf
+open Time
+open NLog
+open NLog.Layouts
+
+let ormonitFileName = Path.Combine(Environment.CurrentDirectory, "Ormonit")
+
+[<EntryPoint>]
+let main argv =
+    let start hc =
+        let psi = new ProcessStartInfo(ormonitFileName, "start")
+        psi.UseShellExecute <- false
+        psi.RedirectStandardOutput <- true
+        psi.RedirectStandardError <- true
+        psi.CreateNoWindow <- true
+        let p = Process.Start(psi)
+        p.BeginErrorReadLine()
+        p.BeginOutputReadLine()
+        p.WaitForExit()
+        p.ExitCode = 0
+
+    let stop hc =
+        let psi = new ProcessStartInfo(ormonitFileName, "stop")
+        psi.UseShellExecute <- false
+        psi.RedirectStandardOutput <- true
+        psi.RedirectStandardError <- true
+        psi.CreateNoWindow <- true
+        let p = Process.Start(psi)
+        p.BeginErrorReadLine()
+        p.BeginOutputReadLine()
+        p.WaitForExit()
+        p.ExitCode = 0
+
+    Service.Default
+    |> with_start start
+    |> with_recovery (ServiceRecovery.Default |> restart (min 10))
+    |> with_stop stop
+    |> run
