f9591e186a8d992d48777eea3603357f63e8aaa1
Octavio Rodríguez <octavio.rdz@outlook.com>
2017-03-19 16:37:48 -0600

    Fix issues caused by rebase master dev

--- src/Ormonit/Infrastructure.fs
+++ src/Ormonit/Infrastructure.fs
@@ -39,46 +39,6 @@ module Logging =
     let setLogFun (logFun : LogLevel -> Func<string> -> exn -> obj array -> unit) =
         logf <- logFun
         ()
-    let private asFunc<'T when 'T : not struct> (value:'T) : Func<'T> =
-        Func<'T>(fun _ -> value)
-    type LogFormat<'T>(logLevel:LogLevel, ex:exn, msgFunc:Func<string>, formatParameters:obj array) =
-        new (logLevel, msgFunc:Func<string>) = LogFormat (logLevel, null, msgFunc, [||])
-        new (logLevel, msg:string) = LogFormat (logLevel, null, asFunc(msg), [||])
-        member x.LogLevel = logLevel
-        member x.MsgFunc = msgFunc
-        member x.Exception = ex
-    type tracel<'T>(ex:exn, msgFunc:Func<string>, formatParameters:obj array) =
-        inherit LogFormat<'T>(LogLevel.Trace, msgFunc)
-        new (ex:exn, msg:string) = tracel (ex, asFunc(msg), [||])
-        new (msg:string) = tracel (null, asFunc(msg), [||])
-        new (msgFunc:Func<string>) = tracel (null, msgFunc, [||])
-    type debugl<'T>(ex:exn, msgFunc:Func<string>, formatParameters:obj array) =
-        inherit LogFormat<'T>(LogLevel.Debug, msgFunc)
-        new (msg:string) = debugl (null, asFunc(msg), [||])
-    
-    type infol<'T>(ex:exn, msgFunc:Func<string>, formatParameters:obj array) =
-        inherit LogFormat<'T>(LogLevel.Info, msgFunc)
-        new (msg:string) = infol (null, asFunc(msg), [||])
-
-    type warnl<'T>(ex:exn, msgFunc:Func<string>, formatParameters:obj array) =
-        inherit LogFormat<'T>(LogLevel.Warn, msgFunc)
-        new (msg:string) = warnl (null, asFunc(msg), [||])
-
-    type errorl<'T>(ex:exn, msgFunc:Func<string>, formatParameters:obj array) =
-        inherit LogFormat<'T>(LogLevel.Error, msgFunc)
-        new (ex:exn, msg:string) = errorl (ex, asFunc(msg), [||])
-        new (msg:string) = errorl (null, asFunc(msg), [||])
-
-    type fatall<'T>(ex:exn, msgFunc:Func<string>, formatParameters:obj array) =
-        inherit LogFormat<'T>(LogLevel.Fatal, msgFunc)
-        new (msg:string) = fatall (null, asFunc(msg), [||])
-
-    let log (logFormat:LogFormat<'T>) : unit =
-        logf logFormat.LogLevel logFormat.MsgFunc logFormat.Exception [||]
-
-    let setLogFun (logFun : LogLevel -> Func<string> -> exn -> obj array -> unit) =
-        logf <- logFun
-        ()
 
     let private asFunc<'T when 'T : not struct> (value:'T) : Func<'T> =
         Func<'T>(fun _ -> value)
